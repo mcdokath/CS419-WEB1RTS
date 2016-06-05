015b1b7bd57538a3cf61e18e590b39892ff52396
mcdokath <mcdokath@oregonstate.edu>
2016-06-05 15:31:03 -0700

    Fixed styling for index.html

--- js/script.js
+++ js/script.js
@@ -2,10 +2,23 @@ $(document).ready(function(){
 	/* NEW GAME is pressed */
 	$("#new-game").click(function() {
 		$("#new-game").hide();
-		$("#load-game").hide();
 		$("#easy-game").show();
 		$("#easy-game").css("display","block");
 		$("#hard-game").show();
 		$("#hard-game").css("display","block");
+	});
+	/* EASY GAME is pressed */
+	$("#easy-game").click(function() {
+		$("#easy-game").hide();
+		$("#hard-game").hide();
+		$("#easy-instructions").show();
+		$("#easy-instructions").css("display","block");
+	});
+	/* HARD GAME is pressed */
+	$("#hard-game").click(function() {
+		$("#easy-game").hide();
+		$("#hard-game").hide();
+		$("#hard-instructions").show();
+		$("#hard-instructions").css("display","block");
 	});
 });
